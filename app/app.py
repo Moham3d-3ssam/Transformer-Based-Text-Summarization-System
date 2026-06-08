86bb8fa3d74a7ffba2ab49baf19d20b495a3ce3b
Mohamed Essam <159931962+Moham3d-3ssam@users.noreply.github.com>
2026-06-08 23:45:52 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -34,7 +34,7 @@ def clean_text(text):
 # =========================
 # Chat function (Streaming)
 # =========================
-def chat(message):
+def chat(message, history):
 
     message = clean_text(message)
 
